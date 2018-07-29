58089a2ba7f2ed59a2c29c50af10e8d3bbded137
Wini19 <29128282+wini19@users.noreply.github.com>
2018-07-29 23:02:02 +0530

    first python program added

---
+++ program1.py
@@ -0,0 +1,38 @@
+#first python program
+#28/07/18
+
+# 1.no need to declare variables
+# 2.variables don't have a fixed datatype
+# 3.strings can be added (concatenated) using '+' sign
+x="ram"
+y="sita"
+print(x + " weds " + y)
+z = x + " and " + y
+print(z + " are married!")
+
+# adding number and string generates error : you can uncomment next two lines to see that error
+# a=x + 10
+# print(a)
+
+# x and y now used to store integers, their datatype is not defined
+# unlike C,java,etc
+x=10
+y=12
+#Indentation play a key role in python
+if y>x :
+    print("y=12 is greater than x=10")
+
+#COMMENTS
+#this is single level "comment" ....we can sometime call it "Documentation"
+"""this
+is
+multi-line
+Documentation"""
+
+#SOME IMP POINTS :
+"""IMP points :
+1.Rules for naming variables in python is same as in C++.
+2.'print()' always prints the contents in new line like println() in Java.
+"""
+
+#Toodles !!! :)
