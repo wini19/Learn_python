d8dcbf5459090acd5869102c91e8805b17803a53
wini19 <29128282+wini19@users.noreply.github.com>
2018-07-29 23:15:50 +0530

    program2.py created : about python numbers

---
+++ program2.py
@@ -0,0 +1,36 @@
+#29/07/18
+
+#Numbers in python : int,float,complex
+
+#int 
+x=1
+y=-78423423842374345
+z=32423467678888883464664 
+print(type(x))
+print(type(y))
+print(type(z))
+print(z-2)
+
+#float 
+x=3.2
+y=0.0
+z=3e5 # 3*10^5
+w=2.001E3
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(w))
+print(y)
+print(z)
+print(w)
+
+#complex nos. : 'j' used
+x=6+5j
+y=8j
+z=-8j
+print(type(x))
+print(type(y))
+print(type(z))
+print(z-2)
+
+
