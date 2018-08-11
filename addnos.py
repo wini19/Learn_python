94d3fdb9a414aeebd8e41103a2f70ef217790872
Wini19 <29128282+wini19@users.noreply.github.com>
2018-08-11 22:42:52 +0530

    add two nos.,exercise added

---
+++ addnos.py
@@ -0,0 +1,8 @@
+# adding two nos
+#https://www.codechef.com/problems/FLOW001
+t=int(input())
+while t>0:
+    n,k=input().split()
+    # n,k = map(int,input().split()) :---"""this can be also used"""
+    print(int(n)+int(k))
+    t=t-1
