f6fca12a50d33ad3f05227df1e93da06ca94f089
Wini19 <29128282+wini19@users.noreply.github.com>
2018-08-18 00:08:49 +0530

    python tuples-updated

--- program7.py
+++ program7.py
@@ -1,15 +1,15 @@
 #python tuples
 #A tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
 t=("is","it","desire") #a tuple
-print(t) 
+print(t)
 print(t[1])
 
 #IMP POINTS :
- """1. You cannot remove items in a tuple.
+"""1. You cannot remove items in a tuple.
     2.You cannot change values in a tuple"""
-    
-t.append("or")
-t[0]="was" #not allowed
+
+# t.append("or") :---not allowed
+# t[0]="was" :------not allowed ""TypeError: 'tuple' object does not support item assignment""
 print(t)
 
 #tuple() constructor
@@ -18,3 +18,4 @@ print(t)
 
 #len() function
 print(len(t))
+# Toodles! :)
